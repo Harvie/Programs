8df8aca072e600350ed5a5a5443b2ffc74dc6163
Harvie <tomas@mudrunka.cz>
2012-04-25 14:52:02 +0200

    getxsock passes arguments to mkxsock

--- c/vizionix/getxsock.sh
+++ c/vizionix/getxsock.sh
@@ -3,7 +3,7 @@ TMPFILE="/tmp/xsock-$USER";
 [ -f "$TMPFILE" ] && . "$TMPFILE"
 [ -n "$XSOCK_PID" ] && kill -0 "$XSOCK_PID" 2>/dev/null || {
 	PATH="./:$PATH"
-	eval "$(mkxsock | tee $TMPFILE)"
+	eval "$(mkxsock $@ | tee $TMPFILE)"
 	echo "XSOCK_PID set to $XSOCK_PID" >&2
 }
 export XSOCK_PID XSOCK_WID

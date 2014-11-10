42b3971be7c95d9205d135cfc90b49669377b8e8
Tomas Mudrunka <tomas@mudrunka.cz>
2014-11-10 14:25:39 +0100

    Added comment

--- c/winsize.c
+++ c/winsize.c
@@ -1,3 +1,7 @@
+/*
+ * Prints size of terminal window in characters
+ */
+
 #include <stdio.h>
 #include <sys/ioctl.h>
 
@@ -8,6 +12,6 @@ static int get_win_size (int fd, struct winsize *win)
 
 int main(void) {
 	struct winsize win;
-	if(!get_win_size(1, &win)) puts("Chyba!");
+	if(!get_win_size(1, &win)) puts("Error!");
 	printf("%d %d :-)\n", win.ws_row, win.ws_col);
 }

b16422a7f8b70676cef7a203f83f5d03ce66e89e
Tomas Mudrunka <tomas@mudrunka.cz>
2016-08-05 22:01:10 +0200

    better getopts

--- bash/getopts.sh
+++ bash/getopts.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-while getopts "a:b:c:d:" OPT; do
+while getopts ":a:b:c:d:f" OPT; do
 	echo "$OPT ==> $OPTARG";
+	case $OPT in
+		a  ) ahoj=$OPTARG;;
+
+		'?') echo "Unknown option: -$OPTARG"			>&2; exit 1;;
+		':') echo "Missing option argument for -$OPTARG"	>&2; exit 2;;
+		*  ) echo "Unimplemented option: -$OPT"			>&2; exit 3;;
+	esac
 done
 
+echo
 shift $(($OPTIND -1)); echo "Rest: $@";
+
+: ${ahoj:=default}
+echo ahoj: $ahoj

b3cc616ba99423231136cf1bdd22e0003d25edc6
Tomas Mudrunka <tomas.mudrunka@gmail.com>
2026-02-10 18:31:23 +0100

    podpora windows

--- python/pydll.py
+++ python/pydll.py
@@ -2,6 +2,6 @@
 
 import ctypes
 from ctypes.util import find_library
-libc = ctypes.CDLL(find_library("libc"))
+libc = ctypes.CDLL(find_library("libc") or find_library("msvcrt"))
 libc.printf(b"TEST: %d %s\n", 1234, b'mujstring')
 
